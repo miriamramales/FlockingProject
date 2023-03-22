1fc92a3f7f7c9d3faaf6e502daff00e69deda8cf
Dori <denmand@augsburg.edu>
2023-03-22 17:24:58 -0500

    fixing errors in controller

--- Lab06/Controller.java
+++ Lab06/Controller.java
@@ -68,7 +68,7 @@ public class Controller implements ActionListener, ItemListener {
     @Override
     public void itemStateChanged(ItemEvent e) {
         // TODO Auto-generated method stub
-        if (e.getSource() == view.c
+    
     }
 
 
